a04e937ad4cae2748f7bba75b1fe18a31c38d696
Giacomo Bubba <giacobub@gmail.com>
2024-07-31 23:38:37 +0200

    bugfix

--- seasonal_color_analysis/core/face_embedding.py
+++ seasonal_color_analysis/core/face_embedding.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 import torch
 import numpy as np
-from facenet_pytorch import MTCNN, InceptionResnetV1, extract_face
+from facenet_pytorch import MTCNN, InceptionResnetV1, extract_face, fixed_image_standardization
 
 
 def select_largest_box(boxes: np.ndarray | None) -> np.ndarray | None:
@@ -10,8 +10,17 @@ def select_largest_box(boxes: np.ndarray | None) -> np.ndarray | None:
         areas = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
         box_order = np.argsort(areas)[::-1]
         return boxes[box_order][0]
+    
+
+def extract_crop(img: Image, box: np.ndarray) -> torch.Tensor | None:
+    if box is not None:
+        face = extract_face(img, box)
+        crop = fixed_image_standardization(face)
+        return crop
 
 class FaceEmbedder:
+    n = 512  # embedding dimension
+
     def __init__(self, embedder: str, device: str = "cpu"):
         self._mtcnn = MTCNN(device=device)
         self._device = device
@@ -23,21 +32,23 @@ class FaceEmbedder:
         # Select faces
         batch_boxes = [select_largest_box(boxes) for boxes in batch_boxes]  # one box per image
         # Extract faces
-        faces = [extract_face(img, box) if box is not None else None for img, box in zip(images, batch_boxes)]  # one crop per image
+        crops = [extract_crop(img, box) if box is not None else None for img, box in zip(images, batch_boxes)]  # one crop per image
         
         # filtering images with no detected faces
-        crops = {i: crop for i, crop in enumerate(faces) if crop is not None}
-        # stacking crops
-        pt_crops = torch.stack(list(crops.values())).to(self._device)
+        crops_dict = {i: crop for i, crop in enumerate(crops) if crop is not None}
+        if crops_dict:
+            # stacking crops
+            pt_crops = torch.stack(list(crops_dict.values())).to(self._device)
 
-        # computing embeddings
-        pt_embeddings = self._resnet(pt_crops)
-        # indexing embeddings
-        embdeddings = {idx: pt_embeddings[i, ...] for i, idx in enumerate(crops.keys())}
-        n = pt_embeddings.shape[-1]
-        # inserting tensor of nan for the images with no detected faces
-        embdeddings_all = [embdeddings[i] if i in embdeddings else torch.ones((n)) * torch.nan for i, _ in enumerate(images)]
-        pt_embeddings_all = torch.stack(embdeddings_all)
+            # computing embeddings
+            pt_embeddings = self._resnet(pt_crops)
+            # indexing embeddings
+            embdeddings = {idx: pt_embeddings[i, ...] for i, idx in enumerate(crops_dict.keys())}
+            # inserting tensor of nan for the images with no detected faces
+            embdeddings_all = [embdeddings[i] if i in embdeddings else torch.ones((self.n)) * torch.nan for i, _ in enumerate(images)]
+            pt_embeddings_all = torch.stack(embdeddings_all)
+        else:
+            pt_embeddings_all = torch.ones((len(images), self.n)) * torch.nan
         return batch_boxes, pt_embeddings_all
 
     def __call__(self, images: list[Image]) -> torch.Tensor:
